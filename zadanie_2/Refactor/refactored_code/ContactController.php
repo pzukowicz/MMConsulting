a6d5605a8ac48053d2cc47bcb4373828927cd810
Zuku101 <patryk180885@gmail.com>
2024-08-29 20:17:36 +0200

    Zadanie rekrutacyjne, 3

--- zadanie_2/Refactor/refactored_code/ContactController.php
+++ zadanie_2/Refactor/refactored_code/ContactController.php
@@ -33,19 +33,33 @@ class ContractController {
   private const ACTION_DISPLAY_CONTRACTS = 5;
 
   /**
+   * Minimum amount for displaying the contract value.
+   *
+   * @var int
+   */
+  private const MIN_AMOUNT_THRESHOLD = 5;
+
+  /**
    * Superglobal request.
    *
    * @var Request
    */
   private Request $request;
 
+  /**
+   * HTTP response object.
+   *
+   * @var ResponseInterface
+   */
   private ResponseInterface $response;
 
   /**
-   * An instance of Request to manage and sanitize superglobal data.
+   * Constructs a ContractController instance.
    *
    * @param PDO $db
    *   Database connection instance.
+   * @param ResponseInterface $response
+   *   Response object.
    */
   public function __construct(PDO $db, ResponseInterface $response) {
     $this->db = $db;
@@ -56,16 +70,19 @@ class ContractController {
   /**
    * Display contracts based on action parameter.
    *
-   * @return void
+   * @return ResponseInterface
+   *   Response with rendered HTML.
    */
-  public function showContracts(): void {
+  public function showContracts(): ResponseInterface {
     $action = $this->request->getGet('akcja');
     $id = $this->request->getGet('id');
     $sort = $this->request->getGet('sort');
 
-    if ($action === ACTION_DISPLAY_CONTRACTS) {
-      $this->displayContracts((int)$id, (int)$sort);
+    if ((int)$action === self::ACTION_DISPLAY_CONTRACTS) {
+      return $this->displayContracts((int)$id, (int)$sort);
     }
+
+    return $this->response;
   }
 
   /**
@@ -76,15 +93,16 @@ class ContractController {
    * @param int $sort
    *   Sort option for ordering contracts.
    *
-   * @return void
+   * @return ResponseInterface
+   *   Response with rendered HTML.
    */
-  private function displayContracts(int $id, int $sort): void {
+  private function displayContracts(int $id, int $sort): ResponseInterface {
     $orderBy = $this->determineOrder($sort);
     $sql = "SELECT * FROM contracts WHERE id = :id AND kwota > 10 {$orderBy}";
     $stmt = $this->db->prepare($sql);
     $stmt->execute(['id' => $id]);
     $contracts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $this->renderContracts($contracts);
+    return $this->renderContracts($contracts);
   }
 
   /**
@@ -116,19 +134,19 @@ class ContractController {
   private function renderContracts(array $contracts): ResponseInterface {
     $html = "<html><body bgcolor='" . self::BGCOLOR . "'><br><table width='95%'>";
     foreach ($contracts as $contract) {
-        $id = htmlspecialchars($contract['id']);
-        $businessName = htmlspecialchars($contract['nazwa_przedsiebiorcy']);
-        $amount = htmlspecialchars($contract['kwota']);
-        $html .= "<tr><td>{$id}</td><td>{$businessName} ";
-        if ($contract['kwota'] > 5) {
-            $html .= "{$amount}";
-        }
-        $html .= "</td></tr>";
+      $id = htmlspecialchars($contract['id']);
+      $businessName = htmlspecialchars($contract['nazwa_przedsiebiorcy']);
+      $amount = htmlspecialchars($contract['kwota']);
+      $html .= "<tr><td>{$id}</td><td>{$businessName} ";
+      if ($contract['kwota'] > self::MIN_AMOUNT_THRESHOLD) {
+        $html .= "{$amount}";
+      }
+      $html .= "</td></tr>";
     }
     $html .= "</table></body></html>";
 
     $this->response->getBody()->write($html);
     return $this->response;
-}
-  
+  }
+
 }
